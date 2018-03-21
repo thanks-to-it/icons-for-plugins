6b4b48f08dce6a66cba841698be59beb2655599e
Pablo S G Pacheco <pablo.sg.pacheco@gmail.com>
2018-03-21 00:36:09 -0300

    asd

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -34,21 +34,6 @@
 
 #!/usr/bin/env bash
 
-if [[ -z "$TRAVIS" ]]; then
-	echo "Script is only to be run by Travis CI" 1>&2
-	exit 1
-fi
-
-if [[ -z "$SVN_PASSWORD" ]]; then
-	echo "WordPress.org password not set" 1>&2
-	exit 1
-fi
-
-if [[ -z "$TRAVIS_BRANCH" || "$TRAVIS_BRANCH" != "master" ]]; then
-	echo "Build branch is required and must be 'master'" 1>&2
-	exit 0
-fi
-
 WP_ORG_USERNAME="$SVN_USERNAME"
 PLUGIN="$PLUGIN_NAME"
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
