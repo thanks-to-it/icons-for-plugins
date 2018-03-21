8ef040df2cb7f60554685e29894094b78eb2505d
Pablo S G Pacheco <pablo.sg.pacheco@gmail.com>
2018-03-20 23:12:19 -0300

    Fix travis 10

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -1,30 +1,30 @@
 #!/usr/bin/env bash
 
 # 1. Clone complete SVN repository to separate directory
-# svn co $SVN_REPOSITORY ../svn
+svn co $SVN_REPOSITORY ../svn
 
 # 2. Copy git repository contents to SNV trunk/ directory
-# cp -R ./* ../svn/trunk/
+cp -R ./* ../svn/trunk/
 
 # 3. Switch to SVN repository
-# cd ../svn/trunk/
+cd ../svn/trunk/
 
 # 4. Move assets/ to SVN /assets/
-# mv ./assets/ ../wporg_assets/
+mv ./wporg_assets/ ../assets/
 
 # 5. Clean up unnecessary files
-# rm -rf .git/
-# rm .travis-deploy.sh
-# rm .travis.yml
+rm -rf .git/
+rm -rf deploy/
+rm .travis.yml
 
 # 6. Go to SVN repository root
-# cd ../
+cd ../
 
 # 7. Create SVN tag
-# svn cp trunk tags/$TRAVIS_TAG
+svn cp trunk tags/$TRAVIS_TAG
 
 # 8. Push SVN tag
-# svn ci  --message "Release $TRAVIS_TAG" \
-#        --username $SVN_USERNAME \
-#        --password $SVN_PASSWORD \
-#        --non-interactive
+svn ci  --message "Release $TRAVIS_TAG" \
+        --username $SVN_USERNAME \
+        --password $SVN_PASSWORD \
+        --non-interactive
