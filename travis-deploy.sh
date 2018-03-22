a02382ac62823fb3b6a249bd5b4834c321f32bb1
Pablo S G Pacheco <pablo.sg.pacheco@gmail.com>
2018-03-21 22:01:39 -0300

    Test

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -10,7 +10,7 @@ cp -R ./* ../svn/trunk/
 cd ../svn/trunk/
 
 # 4. Move assets/ to SVN /assets/
-mv wporg_assets/ ../assets/
+mv ./wporg_assets/* ../assets/
 
 # 5. Clean up unnecessary files
 rm -rf .git/
