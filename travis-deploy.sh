5ebbb238611189bc831fd6080cb30dc0592709a6
Pablo S G Pacheco <pablo.sg.pacheco@gmail.com>
2018-03-21 21:35:30 -0300

    Improve travis setup

--- travis-deploy.sh
+++ travis-deploy.sh
@@ -10,18 +10,18 @@ cp -R ./* ../svn/trunk/
 cd ../svn/trunk/
 
 # 4. Move assets/ to SVN /assets/
-mv ./wporg_assets/ ../assets/
+yes | cp -rf ./wporg_assets/ ../assets/
 
 # 5. Clean up unnecessary files
-rm -rf .git/
-rm travis-deploy.sh
-rm .travis.yml
+rm -rf ./.git/
+rm ./travis-deploy.sh
+rm ./.travis.yml
 
 # 6. Go to SVN repository root
 cd ../
 
 # 7. Create SVN tag
-svn cp trunk tags/$TRAVIS_TAG
+yes | cp -rf trunk tags/$TRAVIS_TAG
 
 # 8. Push SVN tag
 svn ci --no-auth-cache -m "Release $TRAVIS_TAG" --username $SVN_USERNAME --password $SVN_PASSWORD
